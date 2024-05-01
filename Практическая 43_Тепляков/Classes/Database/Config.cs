db086963172194e076655778f076ab38207c186a
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-05-01 19:13:13 +0500

    Реализоаван конфиг подключения к базе. Тепляков

---
+++ Практическая 43_Тепляков/Classes/Database/Config.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Практическая_43_Тепляков.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1;uid=root;database=pr43;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
