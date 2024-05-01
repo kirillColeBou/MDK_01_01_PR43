5b99d6f5bfc9e64cceb18be55472d08648643d32
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-05-01 19:28:34 +0500

    Реализованы контексты для моделей. Тепляков

---
+++ Практическая 43_Тепляков/Context/ContactsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Практическая_43_Тепляков.Context
+{
+    public class ContactsContext : DbContext
+    {
+        public DbSet<Models.Contacts> Contacts { get; set; }
+
+        public ContactsContext()
+        {
+            Database.EnsureCreated();
+            Contacts.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql(Classes.Database.Config.connection, Classes.Database.Config.version);
+    }
+}
---
+++ Практическая 43_Тепляков/Context/GroupsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Практическая_43_Тепляков.Context
+{
+    public class GroupsContext : DbContext
+    {
+        public DbSet<Models.Groups> Groups { get; set; }
+
+        public GroupsContext()
+        {
+            Database.EnsureCreated();
+            Groups.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql(Classes.Database.Config.connection, Classes.Database.Config.version);
+    }
+}
